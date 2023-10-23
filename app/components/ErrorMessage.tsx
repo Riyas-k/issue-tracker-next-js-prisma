2ba2c0d2e9ec741488f2379b5c067e72c6e27bff
Riyazo4 <mohammedriyazriyaz04@gmail.com>
2023-10-24 01:56:14 +0530

    Extract-the Error Message Component

---
+++ app/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import React, { PropsWithChildren } from "react";
+import { Text } from "@radix-ui/themes";
+
+const ErrorMessage = ({ children }: PropsWithChildren) => {
+  if (!children) return null;
+  return (
+    <Text color="red" as="p">
+      {children}
+    </Text>
+  );
+};
+
+export default ErrorMessage;
